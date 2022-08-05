0c6069c2b2e939d1e6177475921ffaca5d33402e
신미래 <sml6209@naver.com>
2022-08-05 10:43:16 +0900

    [feat🛠]: 로그인 유효성 검사 - 이메일, 비밀번호 유효성 검사 - 2가지 input 내용이 모두 조건을 만족해야 버튼 활성화

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,71 @@
+import { useMemo, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { validateEmail } from "utils/validate";
+
 export default function Login() {
-  return <div>로그인</div>;
+  const navigate = useNavigate();
+
+  const [inputEmail, setInputEmail] = useState("");
+  const [inputPw, setInputPw] = useState("");
+
+  const [isValidEmail, setIsValidEmail] = useState(false);
+  const [isValidPw, setIsValidPw] = useState(false);
+
+  const [textValidEmail, setTextValidEmail] = useState("");
+  const [textValidPw, setTextValidPw] = useState("");
+
+  const isValidAll = useMemo(() => {
+    return isValidEmail && isValidPw;
+  }, [isValidEmail, isValidPw]);
+
+  const handleInputEmail = (e) => {
+    setInputEmail(e.target.value);
+
+    if (!validateEmail(e.target.value)) {
+      setTextValidEmail("이메일을 입력해야 해요 :)");
+      setIsValidEmail(false);
+    } else {
+      setTextValidEmail("");
+      setIsValidEmail(true);
+    }
+  };
+
+  const handleInputPw = (e) => {
+    setInputPw(e.target.value);
+
+    if (e.target.value) {
+      setTextValidPw("");
+      setIsValidPw(true);
+    } else {
+      setTextValidPw("비밀번호를 입력해주세요!");
+      setIsValidPw(false);
+    }
+  };
+
+  const moveToSignUp = () => {
+    navigate("/signup");
+  };
+
+  return (
+    <section>
+      <input
+        type="text"
+        value={inputEmail}
+        onChange={handleInputEmail}
+        placeholder="이메일"
+      />
+      <div>{textValidEmail}</div>
+
+      <input
+        type="password"
+        value={inputPw}
+        onChange={handleInputPw}
+        placeholder="비밀번호"
+      />
+      <div>{textValidPw}</div>
+
+      <button disabled={!isValidAll}>로그인</button>
+      <button onClick={moveToSignUp}>회원가입하러 가기</button>
+    </section>
+  );
 }
